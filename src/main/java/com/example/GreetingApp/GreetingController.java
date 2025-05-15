853d099630c678cfb735f32c5e352ce7bb61d914
sanjeevknsty <ksanjeev0359@gmail.com>
2025-05-15 15:23:23 +0530

    uc-1 HTTP Methods commit

---
+++ src/main/java/com/example/GreetingApp/GreetingController.java
@@ -0,0 +1,60 @@
+package com.example.GreetingApp;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+	private static final String template ="Hello,%s";
+	private final AtomicLong counter = new AtomicLong();
+	
+	@GetMapping("/greeting")
+	public Greeting greeting(@RequestParam(value ="name", defaultValue="world") String name) {
+		return new Greeting(counter.incrementAndGet(),String.format(template, name));
+	}
+	@PostMapping("/post")
+	public Greeting greeting1(@RequestBody String name) {
+		return new Greeting(counter.incrementAndGet(),String.format(template, name));
+	}
+	
+	@PutMapping("/put/{name}")
+	public Greeting greeting2(@PathVariable String name) {
+		return new Greeting(counter.incrementAndGet(),String.format(template, name));
+	}
+//	@DeleteMapping("/delete/{name}")
+//	public Greeting greeting3(@PathVariable String name) {
+//		return new Greeting(counter.decrementAndGet(),String.format(template, name));
+//	}
+	@DeleteMapping("/{id}")
+	public String deleteGreeting(@PathVariable Long id) {
+	    return "Greeting with ID " + id + " deleted.";
+	}
+
+	
+}
+
+class Greeting {
+    private final long id;
+    private final String content;
+
+    public Greeting(long id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
